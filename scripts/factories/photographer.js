60abd3b20194a21d3bc7ac54c8bf5e1a1732db1b
ChristellePhilippe <c.philippe04@hotmail.com>
2022-01-12 11:25:34 +0100

    1er jet page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  //creation des cartes photographes sur la page d'accueil
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const villePays = document.createElement("p");
+    villePays.classList.add("villePays");
+    villePays.textContent = city + ", " + country;
+
+    const tag = document.createElement("p");
+    tag.classList.add("tag");
+    tag.textContent = tagline;
+
+    const prix = document.createElement("p");
+    prix.classList.add("prix");
+    prix.textContent = price + "€/jour";
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(villePays);
+    article.appendChild(tag);
+    article.appendChild(prix);
+
+    return article;
+  }
+  return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
